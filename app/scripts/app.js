764e97eacdc7ae12eaa0fc9fb22567a8b681498f
Livius <plivius@yahoo.com>
2015-12-04 20:59:38 +0200

    Boilerplate commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name snippetAppApp
+ * @description
+ * # snippetAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('snippetAppApp', []);
